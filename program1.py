11c2c1a4ec54740270f653c224f8263197da6dd7
RkrWete <rkrwete@yandex.ru>
2024-04-22 22:12:28 +0800

    Изменены столбцы с B_y_GSE  B_z_GSE на GSM. Изменины названия столбцов. Изменен стиль написания кода на PEP8.

--- program1.py
+++ program1.py
@@ -4,61 +4,78 @@ import pandas as pd
 import datetime
 
 # Создаем список дат за два года от апреля 2022 до апреля 2024
-startDate = datetime.datetime(2022, 4, 1)
-endDate = datetime.datetime(2024, 4, 1)
+start_date = datetime.datetime(2022, 4, 1)
+end_date = datetime.datetime(2024, 4, 1)
 
 # Функция для выгрузки данных за период
-def getData(startDate, endDate):
+def get_data(start_date, end_date):
     # Сформировать массив годов
-    startYear = int(startDate.year)
-    endYear = int(endDate.year)
+    start_year = int(start_date.year)
+    end_year = int(end_date.year)
     years = []
-    while startYear <= endYear:
-        years.append(startYear)
-        startYear += 1 
+    while start_year <= end_year:
+        years.append(start_year)
+        start_year += 1 
 
-    dataAllYear = pd.DataFrame()
+    data_all_years = pd.DataFrame()
     for year in years:
-        dataYear = sw.omnie_hourly(int(year), cache=True)
-        dataAllYear = pd.concat([dataAllYear, dataYear], ignore_index=True)
+        data_year = sw.omnie_hourly(int(year), cache=True)
+        data_all_years = pd.concat([data_all_years, data_year], ignore_index=True)
 
     # Выбор необходимых индексов
-    dataAllYear = dataAllYear[['year', 'doy', 'hour', 'Dst', 'AE', 'AL', 'B_x', 'B_y_GSE', 'B_z_GSE',  'v_plasma', 'n_p', 'T_p', 'Kp', 'Ap', 'f107_adj', 'Lya']]
+    data_all_years = data_all_years[[
+        'year', 'doy', 'hour', 'Dst', 'AE', 'AL', 'B_x', 'B_y_GSM', 'B_z_GSM', 
+        'v_plasma', 'n_p', 'T_p', 'Kp', 'Ap', 'f107_adj', 'Lya'
+    ]]
 
     # Преобразование столбцов year, doy, hour во временные метки
-    dataAllYear['date'] = pd.to_datetime(dataAllYear['year'] * 1000 + dataAllYear['doy'], format='%Y%j', errors='coerce')
-    dataAllYear['month'] = dataAllYear['date'].dt.month
-    dataAllYear['day'] = dataAllYear['date'].dt.day
-    dataAllYear['timestamp'] = pd.to_datetime(dataAllYear[['year', 'day', 'month', 'hour']], format='%Y %j %m %H', utc=True).dt.strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = data_all_years['year']  * 1000 + data_all_years['doy']
+    data_all_years['date'] = pd.to_datetime(timestamp, format='%Y%j', errors='coerce')
+    
+    data_all_years['month'] = data_all_years['date'].dt.month
+    data_all_years['day'] = data_all_years['date'].dt.day
+    data_time = data_all_years[['year', 'day', 'month', 'hour']]
+    data_time = pd.to_datetime(data_time, format='%Y %j %m %H', utc=True)
+    data_all_years['data-time'] = data_time.dt.strftime('%Y-%m-%d %H:%M:%S')
 
     # Удалить ненужные столбцы
-    dataAllYear = dataAllYear.drop(columns=['date'])
-    dataAllYear = dataAllYear.drop(columns=['doy'])
-    dataAllYear = dataAllYear.drop(columns=['year'])
-    dataAllYear = dataAllYear.drop(columns=['day'])
-    dataAllYear = dataAllYear.drop(columns=['month'])
-    dataAllYear = dataAllYear.drop(columns=['hour'])
+    data_all_years = data_all_years.drop(columns=['date'])
+    data_all_years = data_all_years.drop(columns=['doy'])
+    data_all_years = data_all_years.drop(columns=['year'])
+    data_all_years = data_all_years.drop(columns=['day'])
+    data_all_years = data_all_years.drop(columns=['month'])
+    data_all_years = data_all_years.drop(columns=['hour'])
 
-    # Переупорядочить столбцы, чтобы 'timestamp' был первым
-    columnOrder = ['timestamp'] + [column for column in dataAllYear if column != 'timestamp']
-    dataAllYear = dataAllYear[columnOrder]
+    # Переименовать столбцы
+    data_all_years.rename(columns={'B_x': 'Bx'}, inplace=True)
+    data_all_years.rename(columns={'B_y_GSM': 'By'}, inplace=True)
+    data_all_years.rename(columns={'B_z_GSM': 'Bz'}, inplace=True)
+    data_all_years.rename(columns={'v_plasma': 'Vsw'}, inplace=True)
+    data_all_years.rename(columns={'n_p': 'Dsw'}, inplace=True)
+    data_all_years.rename(columns={'T_p': 'Tsw'}, inplace=True)
+    data_all_years.rename(columns={'f107_adj': 'F107'}, inplace=True)
+    data_all_years.rename(columns={'Lya': 'Lalfa'}, inplace=True)
+
+    # Переупорядочить столбцы, чтобы 'data-time' был первым
+    columnOrder = ['data-time'] + [column for column in data_all_years if column != 'data-time']
+    data_all_years = data_all_years[columnOrder]
     
     # Cрез по нужному периоду
-    dataAllYear['timestamp'] = pd.to_datetime(dataAllYear['timestamp'])
-    dataAllYear = dataAllYear[(dataAllYear['timestamp'] >= startDate) & (dataAllYear['timestamp'] <= endDate)]
+    data_all_years['data-time'] = pd.to_datetime(data_all_years['data-time'])
+    data_all_years = data_all_years[
+        (data_all_years['data-time'] >= start_date) & 
+        (data_all_years['data-time'] <= end_date)
+    ]
     
     # Обновить индексы
-    dataAllYear.reset_index(drop=True, inplace=True)
+    data_all_years.reset_index(drop=True, inplace=True)
 
-    return dataAllYear
-
-
+    return data_all_years
 
 
+print(get_data(start_date, end_date))
 
-print(getData(startDate, endDate))
-
-# dataAllYear.to_pickle('Data.pkl')
+# data_all_years.to_pickle('Data.pkl')
 
 # import pickle
 # data = pickle.load(open('Data.pkl', 'rb'))
