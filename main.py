9fd72b76bc0aa933af37251f464891f33d6670e4
RkrWete <rkrwete@yandex.ru>
2024-06-18 23:08:43 +0800

    Линейная регресия

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import datetime
 import get_data as gd
 import compare_data as cd
 import analysis_data_gaps as adg
+import linear_regression as lr
 
 # data = pd.read_pickle('data/final_inds.pkl')
 
-start_date = datetime.datetime(1970, 1, 1)
-end_date = datetime.datetime(2022, 6, 30)
+# start_date = datetime.datetime(1970, 1, 1)
+# end_date = datetime.datetime(2022, 6, 30)
+
+start_date = datetime.datetime(2009, 1, 1)
+end_date = datetime.datetime(2020, 1, 1)
 
 initial_data = gd.get_data(start_date, end_date)
 initial_data = gd.transform_data(initial_data)
@@ -20,5 +24,8 @@ initial_data = gd.transform_data(initial_data)
 # print(compare_1hour_data, compare_3hour_data, compare_24hour_data)
 
 # Анализ пропусков
-data = adg.analysis_data_gaps(initial_data, initial_data[2], 'f107', 999)
-print(data)
+# data = adg.analysis_data_gaps(initial_data, initial_data[2], 'f107', 999)
+# print(data)
+
+# Востановление Bz из dst
+print(lr.linear_regression(initial_data[0].dst, initial_data[0].bz))
