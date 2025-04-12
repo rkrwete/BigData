b5af21ce9fc1c1ea1e07047fa2fc2c25a16e2658
RkrWete <rkrwete@yandex.ru>
2025-04-12 19:46:22 +0800

    Программная загрузка SYM и ASY.

--- get_indexes_SYM_ASY.py
+++ get_indexes_SYM_ASY.py
@@ -4,22 +4,22 @@ import subprocess
 
 from datetime import datetime, timedelta
 
-def get_indexes_SYM_ASY():
-    directory = "data/SYM_ASY"
-    files = os.listdir(directory)
+# def get_indexes_SYM_ASY():
+#     directory = "data/SYM_ASY"
+#     files = os.listdir(directory)
 
-    data_frames = []
+#     data_frames = []
 
-    for file in files:
-        df = pd.read_csv(directory + '/' + file, delim_whitespace=True, header=None)
-        data_frames.append(df)
-        df = pd.concat(data_frames, ignore_index=True)
-        df.columns = ['year', 'day_of_year', 'hour', 'minute', 'SYM_D', 'SYM_H', 'ASY_D', 'ASY_H']
-        df = df[df['minute'] == 0]
-        df['datetime'] = pd.to_datetime(df['year'] * 1000 + df['day_of_year'], format='%Y%j') + pd.to_timedelta(df['hour'], unit='h')
-        df.set_index('datetime', inplace=True)
-        df = df[['SYM_D', 'SYM_H', 'ASY_D', 'ASY_H']]
-        df.to_csv('SYM_ASY.csv')
+#     for file in files:
+#         df = pd.read_csv(directory + '/' + file, delim_whitespace=True, header=None)
+#         data_frames.append(df)
+#         df = pd.concat(data_frames, ignore_index=True)
+#         df.columns = ['year', 'day_of_year', 'hour', 'minute', 'SYM_D', 'SYM_H', 'ASY_D', 'ASY_H']
+#         df = df[df['minute'] == 0]
+#         df['datetime'] = pd.to_datetime(df['year'] * 1000 + df['day_of_year'], format='%Y%j') + pd.to_timedelta(df['hour'], unit='h')
+#         df.set_index('datetime', inplace=True)
+#         df = df[['SYM_D', 'SYM_H', 'ASY_D', 'ASY_H']]
+#         df.to_csv('SYM_ASY.csv')
 
 # get_indexes_SYM_ASY()
 
